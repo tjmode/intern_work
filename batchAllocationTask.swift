3ff50b3337e3c95177617c955d7c080cd899df8d
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-03-20 17:51:53 +0530

    Update batchAllocationTask.swift

--- batchAllocationTask.swift
+++ batchAllocationTask.swift
@@ -1,30 +1,45 @@
 import Foundation
-var batch = [["avail": 100, "name": "B1", "caSe":0, "outer": 0, "pieces": 0],
-            ["avail": 125, "name": "B2", "caSe":0, "outer": 0, "pieces": 0],
-            ["avail": 25, "name": "B3", "caSe":0, "outer": 0, "pieces": 0]
+var batch = [["avail": 49, "name": "B1", "caSe":0, "outer": 0, "pieces": 0],
+            ["avail": 25, "name": "B2", "caSe":0, "outer": 0, "pieces": 0],
+            ["avail": 202, "name": "B3", "caSe":0, "outer": 0, "pieces": 0]
 ]
 let caSe = 50
 let outer = 25
 let piece = 1
-var orderedItems = 22512
+var orderedItems = 202
 for products in 0..<batch.count {
     //var available = batch[c]["avail"] as! Int
     if orderedItems >= 50  {
-        if batch[products]["avail"] as! Int >= caSe{
+        if batch[products]["avail"] as! Int >= caSe {
         var howManyCaneCanKeep = orderedItems / caSe
         var howManyCaneCanKeepInThisBatch = batch[products]["avail"] as! Int / caSe
+        if howManyCaneCanKeepInThisBatch >= howManyCaneCanKeep {
+        batch[products]["caSe"] = howManyCaneCanKeep
+        batch[products]["avail"] = batch[products]["avail"] as! Int -  howManyCaneCanKeep * caSe
+        orderedItems = orderedItems - howManyCaneCanKeep * caSe
+        }
+        else {
         orderedItems = orderedItems - howManyCaneCanKeepInThisBatch * caSe
         batch[products]["avail"] = batch[products]["avail"] as! Int -  howManyCaneCanKeepInThisBatch * caSe
         batch[products]["caSe"] = howManyCaneCanKeepInThisBatch
     }
+        }
     }
     if orderedItems >= outer {
-        if batch[products]["avail"] as! Int >= outer{
-        var howManyCaneCanKeepInThisBatch = batch[products]["avail"] as! Int / outer
-         orderedItems = orderedItems - howManyCaneCanKeepInThisBatch *  outer
-         batch[products]["avail"] = batch[products]["avail"] as! Int -  howManyCaneCanKeepInThisBatch *  outer
-         batch[products]["outer"] = howManyCaneCanKeepInThisBatch
-    } 
+               if batch[products]["avail"] as! Int >= outer {
+        let howManyOuterCanKeep = orderedItems / outer
+        var howManyOuterCanKeepInThisBatch = batch[products]["avail"] as! Int / outer
+        if howManyOuterCanKeepInThisBatch >= howManyOuterCanKeep {
+        batch[products]["outer"] = howManyOuterCanKeep
+        batch[products]["avail"] = batch[products]["avail"] as! Int -  howManyOuterCanKeep * outer
+        orderedItems = orderedItems - howManyOuterCanKeep * outer
+        }
+        else {
+        orderedItems = orderedItems - howManyOuterCanKeepInThisBatch * outer
+        batch[products]["avail"] = batch[products]["avail"] as! Int -  howManyOuterCanKeepInThisBatch * outer
+        batch[products]["outer"] = howManyOuterCanKeepInThisBatch
+    }
+        }
     }
     if orderedItems >= piece {
         if batch[products]["avail"] as! Int >= piece {
@@ -42,9 +57,8 @@ for products in 0..<batch.count {
     }
 }
 if orderedItems <= 0 {
-    print(batch)
-   
+    print(batch)  
 }
 else {
- print("Sorry stock Not avaiable for \(orderedItems)")
+ print("Sorry stock Not avaiable for \(orderedItems - 1)")
 }
