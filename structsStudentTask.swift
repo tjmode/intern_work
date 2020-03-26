ce91e0a2fcd55bd457debe5a3ddac0b5c2597b61
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-03-26 19:43:12 +0530

    Update structsStudentTask.swift

--- structsStudentTask.swift
+++ structsStudentTask.swift
@@ -5,11 +5,23 @@ struct students{
     var pythonMark: Int
     var swiftMark: Int
     var cMArk: String 
-    func totalMark() -> Int {
-        var totalMark = (pythonMark + swiftMark) / 2
-        print(totalMark)
+    func totalMark() -> [String] {
+        var totalMark = ["\((pythonMark + swiftMark) / 2)",name]
         return totalMark
     }
+    func allClear() -> Int {
+        let marks = [pythonMark,swiftMark]
+        var rankholders = [Int]()
+        let failedStudents = marks.filter { $0 <= 44 }
+        if failedStudents == [] {
+            var rankholders = marks.reduce(0, +)
+            return rankholders
+        }
+        else { 
+            
+        }
+    }
+
 }
 let studentSheet = ["tony,1, {python:67, swift:45, c:10}","yyy,1, {python:67, swift:95, c:90}","zzz,1, {python:7, swift:55, c:80}"]
 for eachstudent in studentSheet {
@@ -19,8 +31,7 @@ for eachstudent in studentSheet {
     let tempSwiftMark = eachstudent.components(separatedBy: "swift:")
     let swiftMark = tempSwiftMark[1].components(separatedBy: ",")
     let tempCMark = eachstudent.components(separatedBy: "c:")
-    
     let cMark = tempCMark[1].components(separatedBy: ",")
     let ans =  students(name: nameAndId[0], id: Int(nameAndId[1]) ?? 0,pythonMark: Int(pythonMark[0]) ?? 0,swiftMark: Int(swiftMark[0]) ?? 0, cMArk: cMark[0] )
-    print(ans.totalMark())
+    print(ans.allClear())
 }
