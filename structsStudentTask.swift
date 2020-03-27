97637ebd153efe52080fceab4d7c0487da64947d
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-03-27 16:38:44 +0530

    compeletedStudentsMarkCalUsingStruct

--- structsStudentTask.swift
+++ structsStudentTask.swift
@@ -38,13 +38,14 @@ struct Students{
 }
 }
 var rankHoldersMarkList = [Int]()
-let studentSheet = ["tony, 1, email@emm.com,{python: 67, swift: 65, c: 46}",
+let studentSheet = ["tony, 1, email@emm.com,{Python: 67, swift: 65, c: 46}",
                     "yyy, 1, dkd@email.com,{python: 67, swift: 95, c: 90}",
                     "zzz, 1, email@email.com,{python: 75, swift: 55, c: 80}",
                    "mmm, 1, email@emm.com,{python: 6, swift: 65, c: 10}"]
 for eachStudentDataToSplit in studentSheet {
     var eachstudent = eachStudentDataToSplit.replacingOccurrences(of: " ", with: "")
      eachstudent = eachstudent.replacingOccurrences(of: "}", with: "")
+     eachstudent = eachstudent.lowercased()
     let nameAndId = eachstudent.components(separatedBy: ",")
     var tempPythonMark = eachstudent.components(separatedBy: "python:")
     let pythonMark = tempPythonMark[1].components(separatedBy: ",")
