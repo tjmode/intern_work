21fd5102cf2cd2e96ba350774b66f84d07a84e7d
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-03-27 10:30:35 +0530

    Create emailChecking.swift

---
+++ emailChecking.swift
@@ -0,0 +1,13 @@
+import Foundation
+var email = "d@gmail.com"
+var valid = 0
+let checkingAt = email.filter { $0 == "@"}
+let checkingDot = email.filter { $0 == "."} 
+if checkingDot == "." , checkingAt == "@" {
+let checkingBeforeAt = email.components(separatedBy: "@")
+let checkingBeforeDot = checkingBeforeAt[1].components(separatedBy: ".")
+if checkingBeforeDot[0] != "" , checkingBeforeDot[0] != "" ,checkingBeforeAt[0] != "" {
+    valid = valid + 1
+}
+print(valid)
+}
