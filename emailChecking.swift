09d91c8c12c74d1652da946958218db184eb9a1c
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-03-27 10:41:45 +0530

    Update emailChecking.swift

--- emailChecking.swift
+++ emailChecking.swift
@@ -1,12 +1,11 @@
-import Foundation
-var email = "d@gmail.com"
+var email = "d@g.h"
 var valid = 0
 let checkingAt = email.filter { $0 == "@"}
 let checkingDot = email.filter { $0 == "."} 
 if checkingDot == "." , checkingAt == "@" {
 let checkingBeforeAt = email.components(separatedBy: "@")
 let checkingBeforeDot = checkingBeforeAt[1].components(separatedBy: ".")
-if checkingBeforeDot[0] != "" , checkingBeforeDot[0] != "" ,checkingBeforeAt[0] != "" {
+if checkingBeforeDot[0] != "" , checkingBeforeDot[1] != "" ,checkingBeforeAt[0] != "" {
     valid = valid + 1
 }
 print(valid)
