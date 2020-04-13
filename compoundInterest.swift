fa8d45bbbe53a2eea2ff075434454c23a56b83b1
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-04-13 18:03:19 +0530

    compoundInterest

--- compoundInterest.swift
+++ compoundInterest.swift
@@ -1,3 +1,12 @@
+/*There’s bank in the name of Ivy Bank. Bank’s Balance 100,00,00,000.
+
+Person X has an account in Ivy Bank. His/her account number is XXXXXXXXXX.
+
+And Person X taken a loan amount of 25,00,000 for an interest rate of 14% and the loan tenure is 120 months. 
+Every month interest has to be recalculated from the principal amount. At the end of 120 month, 
+how much interest will be paid by the Person X.
+ Also at the end of 120 months what will be the Banks’s balance.
+*/
 import Foundation
 let bankBalance: Double = 1000000000
 func amountCalculation(amount: Float, monthsOfTenure: Int, rateOfIntertrest: Float) -> (Double, Float){
