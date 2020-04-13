85d5d68a7dab4e4ad2363880b275cf6ccc0bcd3e
tony wilson <40241323+tjmode@users.noreply.github.com>
2020-04-13 20:11:23 +0530

    added simple interest

---
+++ bankTask.swift
@@ -0,0 +1,45 @@
+/*There’s bank in the name of Ivy Bank. Bank’s Balance 100,00,00,000.
+
+Person X has an account in Ivy Bank. His/her account number is XXXXXXXXXX.
+
+And Person X taken a loan amount of 25,00,000 for an interest rate of 14% and the loan tenure is 120 months. 
+Every month interest has to be recalculated from the principal amount. At the end of 120 month, 
+how much interest will be paid by the Person X.
+ Also at the end of 120 months what will be the Banks’s balance.
+*/
+import Foundation
+func compoundInterest(amount: Double, monthsOfTenure: Double, rateOfIntertrest: Double) -> (Double){
+    var totalInterestToPay: Double = 0
+    var duplicateAmount: Double = amount
+    for i in 0..<Int(monthsOfTenure) {
+        let interestToPay: Double = Double((duplicateAmount * rateOfIntertrest) / (12 * 100))
+        totalInterestToPay += interestToPay
+        duplicateAmount += interestToPay
+    }
+    return (totalInterestToPay)
+}
+func simpleInterest(amount: Double, monthsOfTenure: Double, rateOfIntertrest: Double) -> (Double){
+   let totalInterestToPay: Double =  amount * (rateOfIntertrest / 100 ) * (monthsOfTenure / 12) 
+   return totalInterestToPay
+}
+let bankBalance: Double = 1000000000
+// loan tenure : example 1 year 2 months, so months = 2 and years = 2 
+let years: Double = 0
+let months: Double = 120
+var monthsOfTenure: Double = months + (years * 12)
+var loanAmount: Double = 2500000
+var isItCompoundInterest = "yes" // YES or NO
+if loanAmount <= bankBalance {
+    if isItCompoundInterest == "yes" {
+        let ans = compoundInterest(amount: Double(loanAmount), monthsOfTenure: monthsOfTenure, rateOfIntertrest: 14)
+        print("interest will be paid by the Person X is \(ans)")
+        print("Banks’s balance \(bankBalance + ans )")
+    } else if  isItCompoundInterest == "no"{
+        let totalInterestToPay = simpleInterest(amount: loanAmount, monthsOfTenure: monthsOfTenure, rateOfIntertrest: 14)
+        print("interest will be paid by the Person X is \(totalInterestToPay)")
+        print("Totalamount Need to pay by x is \(totalInterestToPay + loanAmount)")
+        print("Banks’s balance \(bankBalance + totalInterestToPay )")
+    }
+} else {
+    print("NO Amount in bank")
+}
